732ea1d34b894cc4198aeb545acd127eaf14ebae
lateofrederick <lateofrederick@gmail.com>
2020-01-29 01:19:11 +0000

    created view functions for the various pages

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PersonSignUpForm, PersonLoginForm, NewBlogPost
+
 
 # Create your views here.
 
 
 def homepage(request):
-    return render(request, "navbar.html")
+    return render(request, "homepage.html")
 
 
 def login(request):
-    return render(request, "login.html")
+    form = PersonLoginForm()
+    if form.is_valid():
+        return redirect("homepage.html")
+    context = {
+        'form':form,
+    }
+    return render(request, "login.html",context)
+
+
+def signup(request):
+    form = PersonSignUpForm()
+    context = {
+        'form':form,
+    }
+    return render(request, "signup.html",context)
+
+
+def profile(request):
+    return render(request, "profile.html")
+
+
+def newblog(request):
+    form = NewBlogPost()
+    context = {
+        'form':form,
+    }
+    return render(request, "newpost.html", context)
