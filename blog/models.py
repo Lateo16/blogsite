2218c5022dedc174cf8eeccf50619cf942c0c9fd
lateofrederick <lateofrederick@gmail.com>
2020-01-29 01:18:23 +0000

    added the file field section to Person's class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import re
 
-# Create your models here.
+
+class Person(models.Model):
+    username = models.CharField(max_length=30,unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=30)
+    profile_img = models.FileField(blank=True, upload_to='images/')
+        
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.CharField(max_length=700)
+    date = models.DateField()
+    blogger = models.ForeignKey(Person, on_delete = models.CASCADE)
+
